587bb2831e7a5041355ac1432fb96e7029b3a83c
Dilan <163019915+dilanisik@users.noreply.github.com>
2026-04-06 23:29:23 +0300

    Add files via upload

---
+++ week_7/train.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+import os
+
+# Yolun doğruluğunu kontrol edelim
+yaml_path = r'/home/alice/Desktop/dataset/data.yaml'
+if not os.path.exists(yaml_path):
+    print("HATA: data.yaml dosyası belirttiğin yolda bulunamadı!")
+else:
+    model = YOLO('yolo11n-pose.pt')
+    model.train(data=yaml_path, epochs=10, imgsz=640, device=0)
